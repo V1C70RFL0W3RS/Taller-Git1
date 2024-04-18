1a26b54f24b0d51ff264399cbe784e570bf4695a
Victor Hugo Flores Carvajal <victorhugoflore8@gmail.com>
2024-04-18 15:55:02 -0700

    Definir la clase persona

---
+++ Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_Git
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad {  get; set; }
+        public float Estatura { get; set; }
+        public float Peso { get; set; }
+    }
+}
